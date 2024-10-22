36744f254364446b06118b9cd6847d09c331d132
djoksa4 <vodovardjordje.cavps@gmail.com>
2024-10-22 21:38:18 +0200

    wrote the action

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,31 @@
 "use strict";
-console.log('Hello World');
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@actions/core");
+const github_1 = require("@actions/github");
+async function run() {
+    var _a;
+    // fetch inputs
+    const token = (0, core_1.getInput)("gh-token");
+    const label = (0, core_1.getInput)("label");
+    // initialize Octokit - GitHub API client
+    const octokit = (0, github_1.getOctokit)(token);
+    // get the pull request from the context payload (available since pull request is the event triggering the workflow)
+    const pullRequest = github_1.context.payload.pull_request;
+    try {
+        // throw error if workflow is not ran on a pull request only
+        if (!pullRequest) {
+            throw new Error("This action can only be run on Pull Requests");
+        }
+        // use GitHub API to add the input label to the pull request
+        await octokit.rest.issues.addLabels({
+            owner: github_1.context.repo.owner,
+            repo: github_1.context.repo.repo,
+            issue_number: pullRequest.number,
+            labels: [label]
+        });
+    }
+    catch (error) {
+        (0, core_1.setFailed)((_a = error === null || error === void 0 ? void 0 : error.message) !== null && _a !== void 0 ? _a : "Unknown error");
+    }
+}
+run();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log('Hello World')
+import { getInput, setFailed } from "@actions/core";
+import { context, getOctokit} from "@actions/github";
+
+async function run() {
+    // fetch inputs
+    const token = getInput("gh-token");
+    const label = getInput("label");
+
+    // initialize Octokit - GitHub API client
+    const octokit = getOctokit(token);
+    // get the pull request from the context payload (available since pull request is the event triggering the workflow)
+    const pullRequest = context.payload.pull_request;
+
+    try {
+        // throw error if workflow is not ran on a pull request only
+        if (!pullRequest) {
+            throw new Error("This action can only be run on Pull Requests");
+        }
+
+        // use GitHub API to add the input label to the pull request
+        await octokit.rest.issues.addLabels({
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            issue_number: pullRequest.number,
+            labels: [label]
+        });
+    } catch (error) {
+        setFailed((error as Error)?.message ?? "Unknown error");
+    }
+}
+
+run();
